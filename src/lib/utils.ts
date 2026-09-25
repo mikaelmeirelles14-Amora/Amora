f6e4a796ec592163f836d4bec7f82e288af04de9
mikaelmeirelles14-Amora <mikaelmeirelles14@gmail.com>
2026-09-25 12:52:32 -0300

    chore: scaffold Amora SaaS (src/lib/utils.ts)

---
+++ src/lib/utils.ts
@@ -0,0 +1,1 @@
+export const currency=(value:number)=>new Intl.NumberFormat("pt-BR",{style:"currency",currency:"BRL"}).format(value);export const cn=(...v:Array<string|false|null|undefined>)=>v.filter(Boolean).join(" ");
